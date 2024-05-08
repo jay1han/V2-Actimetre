bf39aae15b4f31d137e4a3c05f8aa47e3575d4c9
Jay Han <jayhan.name@gmail.com>
2024-05-08 22:22:28 +0200

    Board detect

--- boards.cpp
+++ boards.cpp
@@ -259,14 +259,15 @@ void setupBoard() {
         pinMode(15, INPUT_PULLDOWN);
         pinMode(1, INPUT_PULLDOWN);
         pinMode(17, INPUT_PULLUP);
-        if (digitalRead(14) == 1 and digitalRead(15) == 1) my.boardType = BOARD_S3_I2C;
-        else if (digitalRead(14) == 0 and digitalRead(15) == 0) my.boardType = BOARD_S3_NEWBOX;
-        else if (digitalRead(14) == 1 and digitalRead(15) == 0) my.boardType = BOARD_S3_NEWBOX2;
-        else if (digitalRead(17) == 0) my.boardType = BOARD_S3_SOLO;
-        else if (digitalRead(1) == 1) {
+        if (digitalRead(1) == 1) {
             my.boardType = BOARD_S3_ZERO;
             my.ledRGB = LED_GRB;
-        } else {
+        }
+        else if (digitalRead(17) == 0) my.boardType = BOARD_S3_SOLO;
+        else if (digitalRead(14) == 1 and digitalRead(15) == 1) my.boardType = BOARD_S3_I2C;
+        else if (digitalRead(14) == 1 and digitalRead(15) == 0) my.boardType = BOARD_S3_NEWBOX2;
+        else if (digitalRead(14) == 0 and digitalRead(15) == 0) my.boardType = BOARD_S3_NEWBOX;
+        else {
             my.boardType = BOARD_BAD;
         }
         break;
